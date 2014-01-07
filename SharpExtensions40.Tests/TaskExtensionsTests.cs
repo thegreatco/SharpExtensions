d62f2029e3a876bea35b8143c1b262cc27d61f10
Peter Garafano <thegreatco@thegreatco.com>
2014-01-07 16:40:02 -0500

    Added test project for SharpExtensions40

---
+++ SharpExtensions40.Tests/TaskExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SharpExtensions;
+
+namespace SharpExtensions40.Tests
+{
+    [TestFixture]
+    public class TaskExtensionsTests
+    {
+        [Test]
+        public void ToVoid()
+        {
+            Assert.DoesNotThrow(() => Task.Delay(5000).ToVoid());
+        }
+    }
+}
