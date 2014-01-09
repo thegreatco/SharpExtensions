72960d5f94dead42e2070c1b63a218fa682b3934
Peter Garafano <thegreatco@thegreatco.com>
2014-01-09 14:35:39 -0500

    Added Json methods and associated nugets.

---
+++ SharpExtensions45.Json/JsonExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SharpExtensions.Json
+{
+    public static class JsonExtensions
+    {
+        public static string ToJson(this object obj, Formatting formatting = Formatting.Indented)
+        {
+            return JsonConvert.SerializeObject(obj, formatting);
+        }
+
+        public static string ToJson(this object obj, Formatting formatting, JsonSerializerSettings settings)
+        {
+            return JsonConvert.SerializeObject(obj, formatting, settings);
+        }
+
+        public static async Task<string> ToJsonAsync(this object obj, Formatting formatting = Formatting.Indented)
+        {
+            return await JsonConvert.SerializeObjectAsync(obj, formatting);
+        }
+
+        public static async Task<string> ToJsonAsync(this object obj, Formatting formatting, JsonSerializerSettings settings)
+        {
+            return await JsonConvert.SerializeObjectAsync(obj, formatting, settings);
+        }
+
+        public static T FromJson<T>(this string @string)
+        {
+            return JsonConvert.DeserializeObject<T>(@string);
+        }
+
+        public static T FromJson<T>(this string @string, JsonSerializerSettings settings)
+        {
+            return JsonConvert.DeserializeObject<T>(@string, settings);
+        }
+
+        public static T FromJson<T>(this string @string, params JsonConverter[] converters)
+        {
+            return JsonConvert.DeserializeObject<T>(@string, converters);
+        }
+
+        public static async Task<T> FromJsonAsync<T>(this string @string)
+        {
+            return await JsonConvert.DeserializeObjectAsync<T>(@string);
+        }
+
+        public static async Task<T> FromJsonAsync<T>(this string @string, JsonSerializerSettings settings)
+        {
+            return await JsonConvert.DeserializeObjectAsync<T>(@string, settings);
+        }
+    }
+}
