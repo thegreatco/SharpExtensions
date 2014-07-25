7334664a6d41f6263a57bc464a5ec99c2b578024
TheGreatCO <thegreatco@thegreatco.com>
2014-07-25 12:10:53 -0400

    Added new Partition method

--- SharpExtensions40/Properties/AssemblyInfo.cs
+++ SharpExtensions40/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotNetExtensions4.0")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpExtensions")]
+[assembly: AssemblyDescription("A collection of extension methods to make things a bit easier, including Task, Enum, String, and Uri extensions.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNetExtensions4.0")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("TheGreatCO")]
+[assembly: AssemblyProduct("SharpExtensions")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.17")]
-[assembly: AssemblyFileVersion("0.0.17")]
+[assembly: AssemblyVersion("0.0.18")]
+[assembly: AssemblyFileVersion("0.0.18")]
--- SharpExtensions45/Properties/AssemblyInfo.cs
+++ SharpExtensions45/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotNetExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpExtensions")]
+[assembly: AssemblyDescription("A collection of extension methods to make things a bit easier, including Task, Enum, String, and Uri extensions.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNetExtensions")]
+[assembly: AssemblyCompany("TheGreatCO")]
+[assembly: AssemblyProduct("SharpExtensions")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.17")]
-[assembly: AssemblyFileVersion("0.0.17")]
+[assembly: AssemblyVersion("0.0.18")]
+[assembly: AssemblyFileVersion("0.0.18")]
